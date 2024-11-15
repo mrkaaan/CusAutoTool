0b86adaeacd700ca09d103e808f764fb69e08c94
mrkaan <mrkaan@qq.com>
2024-11-15 12:32:31 +0800

    feat: 快捷启动文件更新

--- auto_file.py
+++ auto_file.py
@@ -1,15 +1,32 @@
+import pyperclip
+from pynput import keyboard
 import subprocess
 
 bat_file_path = r'D:\project\auto_customer\auto_file.bat'
-# file_path = r'D:\test.png'
-file_path = r'C:\Users\Kan\Documents\Captura\2024-11-15-11-50-10.mp4'
 
-# 使用 subprocess.run 执行批处理文件
-try:
-    result = subprocess.run([bat_file_path, file_path], check=True)
-    print("批处理文件执行成功")
-except subprocess.CalledProcessError as e:
-    print(f"批处理文件执行失败: {e}")
+# 定义要监听的热字符串及其对应的批处理文件路径
+hotstrings = {
+    'test1': 'D:\\test.png',
+    'test2': r'C:\Users\Kan\Documents\Captura\2024-11-15-11-50-10.mp4'
+}
 
+# 定义一个函数来处理热字符串
+def on_press(key):
+    try:
+        # 检查是否按下了空格键
+        if key == keyboard.Key.space:
+            # 获取当前输入的文本
+            current_text = pyperclip.paste()
+            # 检查当前输入的文本是否是热字符串
+            for hotstring, file_path in hotstrings.items():
+                if current_text.endswith(hotstring):
+                    # 执行对应的批处理文件
+                    subprocess.run([bat_file_path, file_path], check=True)
+                    print(f"Executed {file_path}")
+    except AttributeError:
+        pass
 
-# pip install pynput
+# 监听键盘
+with keyboard.Listener(on_press=on_press) as listener:
+    print("快捷文件监听已启动")
+    listener.join()
