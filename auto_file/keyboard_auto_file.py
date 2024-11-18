f033954e11bff8aff509200e56240212d3265614
mrkaan <mrkaan@qq.com>
2024-11-19 00:49:58 +0800

    feat: 增加自动文件调试信息

--- auto_file/keyboard_auto_file.py
+++ auto_file/keyboard_auto_file.py
@@ -13,6 +13,9 @@ config.read('config.ini')
 bat_file_path = config['default']['BAT_FILE_PATH']
 hotstrings_file_name = config['default']['HOT_FILE_NAME']
 
+print(f"Batch file path: {bat_file_path}")  # 打印批处理文件路径
+print(f"Hotstrings file name: {hotstrings_file_name}")  # 打印热字符串文件名
+
 # 定义要监听的热字符串及其对应的批处理文件路径
 with open(hotstrings_file_name, 'r', encoding='utf-8') as f:
     hotstrings = json.load(f)
@@ -40,14 +43,16 @@ def on_press_clipboard():
             for hotstring in hotstring_set:
                 if current_text.endswith(hotstring):
                     file_path = hotstrings[hotstring]
+                    print(f"Found hotstring: {hotstring}, executing batch file with argument: {file_path}")
                     # 在独立线程中执行批处理文件
                     threading.Thread(target=execute_bat, args=(bat_file_path, file_path)).start()
-                    print(f"Executed {file_path}")
 
 def execute_bat(bat_file_path, file_path):
     try:
         result = subprocess.run([bat_file_path, file_path], check=True, capture_output=True, text=True)
         print(f"Batch file executed successfully: {result.stdout}")
+    except FileNotFoundError:
+        print(f"Error: The batch file was not found at {bat_file_path}.")
     except subprocess.CalledProcessError as e:
         print(f"Error executing batch file: {e}, Output: {e.output}")
 
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from WinGUI import WinGUI
 import organize_table as tb
 import automation as au
 import utils as ut
-from auto_file.auto_file import auto_file as af
 
 # 标准库
 import math       # 提供数学函数，例如三角函数、对数、幂运算等
@@ -46,16 +45,7 @@ if __name__ == "__main__":
         {'key': 'ctrl+shift+o', 'func': au.run_once_remarks_by_qianniu, 'args': [window_name]},
         {'key': 'ctrl+shift+u', 'func': au.run_once_unmark_by_qianniu, 'args': [window_name]}
     ]
-    # ut.auto_key_with_threads(hotkey_actions)
-
-    try:
-        af.start_listener()
-        while True:
-            pass
-    except KeyboardInterrupt:
-        print('Exiting...')
-    finally:
-        af.stop_listener()
+    ut.auto_key_with_threads(hotkey_actions)
 
     # ---------- 测试 -------------
     # au.run_test(window_name)
